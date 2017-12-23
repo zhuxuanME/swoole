4bcb98778bfd09413334dee56a8a5fd01a2d63c1
zhuxuanME <email@zhuxuan.me>
2017-12-24 02:09:46 +0800

    Add files via upload

---
+++ js/dialog.js
@@ -0,0 +1,65 @@
+$(function() {
+  var $dialogContent = $('.dialog-content'),
+      $contnetBox = $('.content-box');
+  // 发送消息
+  $('.input-box').on('keypress',function(e) {
+    var e = e || window.event;
+    var tpl = `<div class="msg-box clearfix">
+        <div class="avatar pull-right">
+          <img src="./images/cat.png" alt="用户头像" width="30" height="30" />
+        </div>
+        <p class="msg-content pull-right">{{msg}}</p>
+      </div>`;
+    var msgContent = $(this).val().trim();
+    if (e.keyCode === 13 && !e.ctrlKey) {
+      // 去除 enter 后的换行
+      e.preventDefault()
+      if(msgContent.length === 0) {
+        alert('你输入的消息不能为空!!!');
+        return;
+      }
+      websocket.send(msgContent);
+      // alert(msgContent)
+      tpl = tpl.replace(/{{\s*msg\s*}}/,msgContent);
+      $('.msg-box').last().after(tpl);
+      $(this).val('');
+      var scrollX = $contnetBox.height() + 40 + 2 - $dialogContent.height(); // 2 header inputbox border
+      $dialogContent.scrollTop(scrollX+500);
+    }
+
+  });
+  // 拖动窗口
+  var down = false;
+  $('.dialog-header').on('mousedown',function(e) {
+    var e = e || window.event,
+        $dialogWrapper = $('.dialog-wrapper');
+    down = true;
+    var clickX = e.pageX,
+        clickY = e.pageY,
+        initX = $dialogWrapper.css("left"),
+        initY = $dialogWrapper.css("top"),
+        outerX = window.innerWidth- $dialogWrapper.outerWidth(),
+        outerY = window.innerHeight- $dialogWrapper.outerHeight();
+      $(this).on('mousemove',function(e) {
+        if(!down) return;
+      var e = e || window.event;
+      var tempX = parseInt(e.pageX) - parseInt(clickX) + parseInt(initX),
+          tempY = parseInt(e.pageY) - parseInt(clickY) + parseInt(initY);
+
+      tempX = tempX <= 0 ? 0 : tempX;
+      tempX = tempX - outerX >= 0 ? outerX : tempX;
+      tempY = tempY <= 0 ? 0 : tempY;
+      tempY = tempY - outerY>=  0? outerY : tempY;
+
+
+      $dialogWrapper.css("left", tempX).css("top", tempY);
+    })
+  })
+  $(document).on('mouseup',function() {
+    down = false;
+  });
+  // 关闭窗口
+  $('.close').click(function() {
+    $('.dialog-wrapper').fadeOut().delay(1000).fadeIn()
+  });
+});
---
+++ server.php
@@ -0,0 +1,33 @@
+<?php
+// 官网demo
+$ws = new swoole_websocket_server('0.0.0.0',8091);
+
+function broadcast(swoole_websocket_server $ws, $msg, $me = null) {
+    foreach($ws->connections as $fd) {
+        if($fd != $me) {
+            $ws->push($fd, $msg);
+        }
+    }
+}
+
+// 监听WebSocket连接打开事件
+$ws->on('open', function (swoole_websocket_server $ws, $request) {
+    $ws->push($request->fd, "欢迎接入miss聊天平台\n");
+    broadcast($ws, "用户{$request->fd}登录啦！", $request->fd);
+});
+
+// 监听WebSocket消息事件
+$ws->on('message', function (swoole_websocket_server $ws, $frame) {
+    $msg =  'from'.$frame->fd.":{$frame->data}\n";
+    broadcast($ws, $msg);
+   // $ws->push($frame->fd, "server: {$frame->data}");
+    // $ws->push($frame->fd, "server: {$frame->data}");
+});
+
+// 监听WebSocket连接关闭事件
+$ws->on('close', function (swoole_websocket_server $ws, $fd) {
+    broadcast($ws, "用户{$fd}下线啦！");
+});
+
+$ws->start();
+?>
